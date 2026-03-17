2b58998ee6db7a11880ff5cfcd5c251a1439602c
duckey <erbstroke@gmail.com>
2026-03-17 23:01:10 +0600

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         HashMap<String, Student> students = new HashMap<>();
@@ -10,39 +7,102 @@ public class Main {
         // ====================== TASK 1 ======================
         // TODO: Добавь минимум 5 студентов (ключ = ID)
         // Сделай минимум два студента с одинаковым GPA (для Task 3)
+        students.put("111",new Student("Nurs",2.7,17));
+        students.put("222",new Student("Azamat",1.0,20));
+        students.put("333",new Student("Bekbolot",2.3,22));
+        students.put("444",new Student("Erbol",4.0,19));
+        students.put("555",new Student("Abylaı",3.2,23));
+
 
         // TODO: Напечатай всех студентов (ID + объект)
+        for(HashMap.Entry<String,Student> student : students.entrySet()){
+            System.out.println("ID : "+student.getKey()
+                    +" , "+student.getValue());
+        }
 
         // TODO: Найди студента по ID и выведи его
+        String findID="222";
+        System.out.println("\nFound Student :");
+        System.out.println(students.get(findID));
+
 
         // TODO: Удали одного студента по ID
+        String findRemoveID="333";
+        System.out.println("\nRemoved Student :");
+        System.out.println(students.remove(findRemoveID));
+
 
         // TODO: Обнови GPA у одного студента
+        System.out.println("\nUpdated GPA Student :");
+        Student s = students.get(findID);
+        s.setGpa(4.0);
+        System.out.println(s);
+
 
         // ====================== SORTING (IMPORTANT) ======================
         // TODO: Создай ArrayList из всех студентов (students.values())
+        ArrayList<Student> studentsList = new ArrayList<>(students.values());
+        System.out.println("\nStudents :");
+        for(Student student : studentsList){
+            System.out.println(student);
+        }
 
         // TODO 6a: Отсортируй по GPA (natural ordering) и выведи
+        System.out.println("\nSorted by GPA :");
+        Collections.sort(studentsList);
+        System.out.println(studentsList);
+
         // TODO 6b: Отсортируй по имени (Comparator) и выведи
+        System.out.println("\nSorted by Name :");
+        studentsList.sort(new NameCompare());
+        System.out.println(studentsList);
+
 
         // ====================== TASK 2 ======================
         System.out.println("\n=== Task 2: Top 3 by GPA ===");
         // TODO: Создай новый список, отсортируй по GPA по убыванию, выведи первые 3
+        ArrayList<Student> top = new ArrayList<>(students.values());
+        Collections.sort(top);
+        for(int i = 0;i<Math.min(3,top.size());i++){
+            System.out.println(top.get(i));
+        }
+
 
         // ====================== TASK 3 ======================
         System.out.println("\n=== Task 3: Students with same GPA ===");
         // TODO: Сгруппируй студентов по GPA и выведи только те, где больше 1 студента
+        HashMap<Double, List<Student>> gpaMap = new HashMap<>();
+
+        for (Student student : students.values()) {
+            gpaMap.putIfAbsent(student.getGpa(), new ArrayList<>());// if no key add
+            gpaMap.get(student.getGpa()).add(student);
+        }
+
+        for (HashMap.Entry<Double, List<Student>> entry : gpaMap.entrySet()) {
+            if (entry.getValue().size() > 1) {
+                System.out.println("GPA: " + entry.getKey());
+                entry.getValue().forEach(System.out::println);
+            }
+        }
 
         // ====================== TASK 4 ======================
         System.out.println("\n=== Task 4: Courses ===");
         HashMap<Course, List<Student>> courseMap = new HashMap<>();
         // TODO: Создай 2–3 курса, добавь студентов, выведи всё
+        Course c1 = new Course("Java");
+        Course c2 = new Course("Python");
+
+        courseMap.put(c1,new ArrayList<>(Arrays.asList(students.get("111"),students.get("222"))));
+        courseMap.put(c2,new ArrayList<>(Arrays.asList(students.get("555"),students.get("444"))));
+
+        for(HashMap.Entry<Course,List<Student>> course : courseMap.entrySet()){
+            System.out.println(course.getKey()+" : "+course.getValue());
+        }
 
         // ====================== TASK 5 ======================
         System.out.println("\n=== Task 5: GPA desc + Name ===");
         // TODO: Создай Comparator (GPA убывание → если равно, то имя возрастание) и отсортируй
+        studentsList.sort(new GpaNameCompare());
+        System.out.println(studentsList);
     }
 }
-
-
-
--- src/Student.java
+++ src/Student.java
@@ -1,3 +1,5 @@
+import java.util.Comparator;
+
 public class Student implements Comparable<Student> {
     private String name;
     private double gpa;
@@ -6,6 +8,9 @@ public class Student implements Comparable<Student> {
     // TODO: Task 1 — Создай конструктор, который принимает имя, GPA и возраст
     public Student(String name, double gpa, int age) {
         // заглушка
+        this.name=name;
+        this.gpa=gpa;
+        this.age=age;
     }
 
     public String getName() { return name; }
@@ -15,18 +20,39 @@ public class Student implements Comparable<Student> {
     // TODO: Task 1 — Добавь возможность изменить GPA
     public void setGpa(double newGpa) {
         // заглушка
+        if(newGpa>=0 && newGpa<=4){
+            this.gpa=newGpa;
+        }
     }
 
     // TODO: Task 1 (Sorting) — Реализуй Comparable по GPA (по возрастанию)
     @Override
     public int compareTo(Student other) {
         // заглушка — верни правильное значение сравнения
-        return 0;
+        return Double.compare(this.gpa,other.gpa);
     }
 
     @Override
     public String toString() {
         // TODO: Сделай красивый вывод
-        return "";
+        return "Name : "+this.name
+                +" , Age : "+this.age
+                +" , GPA : "+this.gpa;
     }
+}
+
+class NameCompare implements Comparator<Student>{
+    public int compare(Student s1 , Student s2){
+        return s1.getName().compareTo(s2.getName());
+    }
+}
+
+class GpaNameCompare implements Comparator<Student>{
+    public int compare(Student s1 , Student s2){
+        int result = Double.compare(s2.getGpa(),s1.getGpa());
+        if(result==0){
+            return s1.getName().compareTo(s2.getName());
+        }return result ;
+    }
+
 }
